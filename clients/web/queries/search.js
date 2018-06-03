867481e5ce8bc8dc15403553cb095690179e57de
darryl-snow <dazsnow@gmail.com>
2018-06-03 22:30:59 +0800

    setup testing

--- clients/web/queries/search.js
+++ clients/web/queries/search.js
@@ -1,4 +1,4 @@
-import gql from 'graphql-tag';
+import gql from 'graphql-tag'
 
 export default gql`
   query SearchQuery($title: String!) {
@@ -12,4 +12,4 @@ export default gql`
       airDate
     }
   }
-`;
+`
